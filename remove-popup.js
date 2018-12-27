ab11cff17830de8c16376e3181703bedb0fcfe1b
Emile Fokkema <emilefokkema@gmail.com>
2018-12-27 14:45:57 +0100

    nothing happens automatically

--- remove-popup.js
+++ remove-popup.js
@@ -1,16 +1,4 @@
 (function(){
-	var throttle = function(f, interval){
-		var busy = false;
-		return function(){
-			if(!busy){
-				busy = true;
-				setTimeout(function(){
-					f();
-					busy = false;
-				}, interval);
-			}
-		};
-	};
 	var kebabCase = function(camelCase){
 		return camelCase.replace(/(?<=[a-z])[A-Z]/g, function(m){return "-"+m.toLowerCase();});
 	};
@@ -114,17 +102,12 @@
 			removeElementsWithCss(x, y, {position: "fixed"});
 		}
 	};
-	act = throttle(act, 500);
 
 	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 		if(request.removePopup){
 			act();
 		}
 	});
-
-	document.addEventListener('DOMNodeInserted',function(){act();})
-
-	act();
 
 })();
 
